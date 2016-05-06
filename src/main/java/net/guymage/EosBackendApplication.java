1c789e6fbc005ca5d663f1505a74768bcfe0e712
Guymage <gdonars@gmail.com>
2016-05-06 23:32:31 +1100

    feat(auth): cors et authentif

--- src/main/java/net/guymage/EosBackendApplication.java
+++ src/main/java/net/guymage/EosBackendApplication.java
@@ -2,11 +2,27 @@ package net.guymage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class EosBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EosBackendApplication.class, args);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/race");
+				registry.addMapping("/race/*").allowedMethods("GET", "HEAD", "POST", "OPTIONS", "DELETE").allowCredentials(true);
+			}
+		};
 	}
 }
--- src/main/java/net/guymage/WebSecurityConfig.java
+++ src/main/java/net/guymage/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -11,10 +12,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.addFilterBefore(new MyCorsFilter(), ChannelProcessingFilter.class);
 		http.authorizeRequests().anyRequest().fullyAuthenticated();
 		http.csrf().disable();
 		http.httpBasic();
-		
 	}
 
 }
